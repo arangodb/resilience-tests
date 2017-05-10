96615a9d8835e2b30ea222d333b337e43a0c581f
Andreas Streichardt <andreas@arangodb.com>
2017-05-10 10:48:10 +0200

    Allow more retries

--- test/shard-move.js
+++ test/shard-move.js
@@ -45,7 +45,7 @@ describe("Move shards", function() {
       }
 
       let waitShardMoved = function(newLeader, numServers, num) {
-        if (num > 500) {
+        if (num > 2000) {
           return Promise.reject(new Error(
             "Shard did not come into sync after " +
               num +
