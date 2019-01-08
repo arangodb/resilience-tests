88fa2bb9af9a75986226e082960e2fb34e471c16
Tobias Gödderz <tobias@arangodb.com>
2019-01-08 14:36:55 +0100

    Rewrote single/agencyfail.js with async/await

--- test/single/agencyfail.js
+++ test/single/agencyfail.js
@@ -7,66 +7,72 @@ const endpointToUrl = InstanceManager.endpointToUrl;
 const rp = require("request-promise-native");
 const arangojs = require("arangojs");
 const expect = require("chai").expect;
+const _ = require('lodash');
+
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+const debugLog = (...args) => {
+  if (process.env.LOG_IMMEDIATE === "1") {
+    console.log(new Date().toISOString(), ' ', ...args);
+  }
+};
 
 // <copied from agency.js>
-let agencyRequest = function(options) {
-  options.followRedirects = options.followRedirects || false;
-  return rp(options)
-    .then(response => {
-      return response;
-    })
-    .catch(err => {
-      if (err.statusCode === 307) {
-        options.url = err.response.headers["location"];
-        return agencyRequest(options);
-      } else if (err.statusCode === 503 && err.message === "No leader") {
-        options.retries = (options.retries || 0) + 1;
-        if (options.retries < 5) {
-          return agencyRequest(options);
-        }
-        return Promise.reject(new Error("no leader after 5 retries"));
+const agencyRequest = async function(options) {
+  options.followAllRedirects = true;
+
+  for (let tries = 0; tries < 5; tries++) {
+    try {
+      return await rp(options);
+    } catch (err) {
+      if (err.statusCode === 503 && err.message === "No leader") {
+        // This may happen, just retry
       }
-      return Promise.reject(err);
-    });
+      else {
+        // Abort when encountering other errors
+        throw err;
+      }
+    }
+  }
+
+  throw new Error("no leader after 5 retries");
 };
 
-let waitForLeader = function(agents) {
-  return rp({
-    url: endpointToUrl(agents[0].endpoint) + "/_api/agency/config",
-    json: true
-  })
-    .then(result => {
-      if (result.leaderId === "") {
-        return Promise.reject(new Error("no leader"));
-      }
-      return result.leaderId;
-    })
-    .catch(() => {
-      return new Promise((resolve, reject) => {
-        setTimeout(resolve, 100);
-      }).then(() => {
-        return waitForLeader(agents);
-      });
-    });
-};
+const waitForLeader = async function(agents) {
+  const isNonEmptyString = x => _.isString(x) && x !== '';
 
-let getLeadAgentInstance = function(agents) {
-  return waitForLeader(agents).then(leaderId => {
-    return agents.reduce((leaderInstance, agent) => {
-      if (leaderInstance) {
-        return leaderInstance;
-      }
-
-      return agencyRequest({
-        url: agent.endpoint + "/_api/agency/config",
+  for (
+    const start = Date.now();
+    Date.now() - start < 30e3;
+    await sleep(100)
+  ) {
+    try {
+      const result = await rp({
+        url: endpointToUrl(agents[0].endpoint) + "/_api/agency/config",
         json: true
-      }).then(result => {
-        if (result.configuration.id === leaderId) {
-          return agent;
-        }
       });
-    });
+      if (isNonEmptyString(result.leaderId)) {
+        return result.leaderId;
+      }
+    } catch(err) {
+    }
+  }
+};
+
+const getLeaderInstance = async function(agents) {
+  debugLog("getting leader instance");
+  const leaderId = await waitForLeader(agents);
+  const getAgencyConfig = agent => agencyRequest({
+    url: endpointToUrl(agent.endpoint) + "/_api/agency/config",
+    json: true
   });
+
+  for (const agent of agents) {
+    const result = await getAgencyConfig(agent);
+    if (result.configuration.id === leaderId) {
+      return agent;
+    }
+  }
 };
 // </copied from agency.js>
 
@@ -79,17 +85,20 @@ describe("Leader-Follower failover + agency outage", async function() {
     agents = await instanceManager.startAgency({ agencySize: 3 });
   });
 
-  afterEach(function() {
+  afterEach(async function() {
     const currentTest = this.ctx ? this.ctx.currentTest : this.currentTest;
     const retainDir = currentTest.state === "failed";
     instanceManager.moveServerLogs(this.currentTest);
-    return instanceManager.cleanup(retainDir).catch(() => {});
+    try {
+      await instanceManager.cleanup(retainDir);
+    } catch(e) {
+    }
   });
 
   // Actual data synchronization
   describe("with data transfer", async function() {
     async function generateData(db, num) {
-      let coll = await db.collection("testcollection");
+      const coll = await db.collection("testcollection");
       await coll.create();
       return Promise.all(
         Array.apply(0, Array(num))
@@ -99,18 +108,18 @@ describe("Leader-Follower failover + agency outage", async function() {
     }
 
     async function checkData(db, num) {
-      let cursor = await db.query(`FOR x IN testcollection
+      const cursor = await db.query(`FOR x IN testcollection
                                     SORT x.test ASC RETURN x`);
       expect(cursor.hasNext()).to.equal(true);
       let i = 0;
       while (cursor.hasNext()) {
-        let doc = await cursor.next();
+        const doc = await cursor.next();
         expect(doc.test).to.equal(i++);
       }
       expect(i).to.equal(num);
     }
 
-    let numDocs = 10000;
+    const numDocs = 10000;
     [2, 4].forEach(n => {
       let f = n / 2;
       it(`with ${n} servers, ${f} failover, server is restarted`, async function() {
@@ -128,7 +137,7 @@ describe("Leader-Follower failover + agency outage", async function() {
         await generateData(db, numDocs);
 
         for (; f > 0; f--) {
-          console.log("Waiting for tick synchronization...");
+          debugLog("Waiting for tick synchronization...");
           const inSync = await instanceManager.asyncReplicationTicksInSync(
             120.0
           );
@@ -142,14 +151,14 @@ describe("Leader-Follower failover + agency outage", async function() {
             uuid
           );
 
-          let leadAgent = await getLeadAgentInstance(agents);
+          const leadAgent = await getLeaderInstance(agents);
 
-          console.log("killing leader %s", leader.endpoint);
+          debugLog("killing leader %s", leader.endpoint);
           await Promise.all([
             instanceManager.kill(leader),
             instanceManager.kill(leadAgent)
           ]);
-          let old = leader;
+          const old = leader;
 
           // use a 60s timeout for selecting a new leader
           uuid = await instanceManager.asyncReplicationLeaderSelected(uuid, 60);
@@ -166,7 +175,7 @@ describe("Leader-Follower failover + agency outage", async function() {
             instanceManager.restart(leadAgent)
           ]);
 
-          console.log("killed instances restarted");
+          debugLog("killed instances restarted");
         }
       });
     });
