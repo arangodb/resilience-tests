9a04500cb6f139aaf312a1f13b959536d0873329
Michael Hackstein <michael@arangodb.com>
2019-07-16 15:13:14 +0200

    Fixed version, did run green loacally \o/

--- test/cluster/replication.js
+++ test/cluster/replication.js
@@ -17,8 +17,13 @@ describe("Replication", function() {
   const instanceManager = InstanceManager.create();
   let db;
 
-  const getInfoFromAgency = async function(path) {
+  const getMultiInfoFromAgency = async function(path) {
     // Path needs to be an array e.g. ["Plan", "Collections", "_system"]
+    // Or an array of above Paths
+    if (!Array.isArray(path) || !Array.isArray(path[0])) {
+      throw "Paths needs to be an array of arrays. Test code wrong";
+    }
+    const body = path.map(p => `/arango/${p.join("/")}`);
     const data = await InstanceManager
     .rpAgency({
       method: "POST",
@@ -26,10 +31,24 @@ describe("Replication", function() {
         instanceManager.getEndpointUrl(instanceManager.agents()[0]) +
         "/_api/agency/read",
       json: true,
-      body: [[`/arango/${path.join("/")}`]]
+      body: [body]
     });
-    // Same as data[0].arango.(path.join(".")) evaluated
-    return path.reduce((obj, attribute) => obj[attribute], data[0].arango);
+
+   // Same as data[0].arango.(path.join(".")) evaluated
+    return path.map(p => p.reduce((obj, attribute) => obj[attribute], data[0].arango));
+  };
+
+  const getInfoFromAgency = async function(path) {
+    // Path needs to be an array e.g. ["Plan", "Collections", "_system"]
+    // Or an array of above Paths
+    if (!Array.isArray(path)) {
+      throw "Path needs to be an array. Test code wrong";
+    }
+    return (await getMultiInfoFromAgency([path]))[0];
+  };
+
+  const getCurrentFailoverCandidates = async () => {
+    return await getInfoFromAgency(["Current", "Collections", "_system", replicationCollectionId, shardName, "failoverCandidates"]);
   };
   
   
@@ -87,6 +106,7 @@ describe("Replication", function() {
       if (name !== leaderName && !offlineFollowers.has(name)) {
         console.log("Shut down : " + name);
         await instanceManager.shutdown(dbServer);
+        console.log("Server : " + name + " is now offline.");
         offlineFollowers.add(name);
         return;
       }
@@ -110,6 +130,52 @@ describe("Replication", function() {
   async function continueServer(dbServer) {
     return await instanceManager.sigcontinue(dbServer);
   }
+
+  const validateInsyncAndFocInLine = async () => {
+    const [foc, insync] = await getMultiInfoFromAgency([
+      ["Current", "Collections", "_system", replicationCollectionId, shardName, "failoverCandidates"],
+      ["Current", "Collections", "_system", replicationCollectionId, shardName, "servers"]
+    ]);
+    expect(insync).to.eql(foc);
+  }
+  
+  const validateROMode = async (availableFollowers, lastLeader) => {
+    let changedLeader = false;
+    for (let i = 0; i < 100; ++i) {
+      const candidates = await getCurrentFailoverCandidates();
+       // We are NEVER allowed to get to less then minReplicationFactor candidates
+      expect(candidates).to.have.length.above(2);
+      try {
+        // TRY to insert
+        await db.collection(replicationCollectionName).save({ testung: Date.now() });
+        // If we can write we must be upgraded to more than 2
+        const inSync = await getCurrentInSyncFollowers();
+        if (inSync[0] !== lastLeader) {
+          changedLeader = true;
+        }
+        expect(changedLeader).to.be.true;
+        if (changedLeader && inSync.length === availableFollowers) {
+          await validateInsyncAndFocInLine();
+          // We stop as soon as we reach the given number of availableFollowers
+          // really in sync
+          const foC = await getCurrentFailoverCandidates();
+          expect(inSync).to.eql(foC);
+          return;
+        }
+      } catch (e) {
+        if (!e.isArangoError) {
+          // Assertion!
+          throw e;
+        }
+        expect(e.isArangoError).to.be.true;
+        expect(e.errorNum).to.equal(1004);
+        expect(e.statusCode).to.equal(403);
+      }
+      await sleep(1000);
+    }
+  };
+
+
 /*
  *  Actual test case section
  */
@@ -139,26 +205,33 @@ describe("Replication", function() {
     let c1 = await db.collection(replicationCollectionName).save({ testung: Date.now() });
     expect(c1).of.be.not.null;
     await shutdownFollower();
-    // Failover cannot have happened yet
+    // We report only after a write
+    expect(await getCurrentFailoverCandidates()).to.have.length(5);
     expect(await getCurrentInSyncFollowers()).to.have.length(5);
+
     // 4 of 5 DBServers left, write to a collection should work flawlessly.
     let c2 = await db.collection(replicationCollectionName).save({ testung: Date.now() });
     expect(c2).of.be.not.null;
+
     // The above has triggered a drop
+    expect(await getCurrentFailoverCandidates()).to.have.length(4);
     expect(await getCurrentInSyncFollowers()).to.have.length(4);
 
     await shutdownFollower();
     // Failover cannot have happened yet
+    expect(await getCurrentFailoverCandidates()).to.have.length(4);
     expect(await getCurrentInSyncFollowers()).to.have.length(4);
 
     // 3 of 5 DBServers left, write to a collection should work flawlessly.
     let c3 = await db.collection(replicationCollectionName).save({ testung: Date.now() });
     expect(c3).of.be.not.null;
     // The above has triggered a drop
+    expect(await getCurrentFailoverCandidates()).to.have.length(3);
     expect(await getCurrentInSyncFollowers()).to.have.length(3);
 
     await shutdownFollower();
     // Failover cannot have happened yet
+    expect(await getCurrentFailoverCandidates()).to.have.length(3);
     expect(await getCurrentInSyncFollowers()).to.have.length(3);
     let firstInsert = false;
     // 2 of 5 DBServers left, write to a collection should fail. Collection is now in read-only mode.
@@ -166,6 +239,7 @@ describe("Replication", function() {
       await db.collection(replicationCollectionName).save({ testung: Date.now() });
       // The above is required to trigger the failover (latest)
       // It is actually okay to NOT get here, this is a race, both cases are covered.
+      expect(await getCurrentFailoverCandidates()).to.have.length(2);
       expect(await getCurrentInSyncFollowers()).to.have.length(2);
       firstInsert = true;
       await db.collection(replicationCollectionName).save({ testung: Date.now() });
@@ -175,10 +249,12 @@ describe("Replication", function() {
       expect(e.errorNum).to.equal(1004);
       expect(e.statusCode).to.equal(403);
     }
+    expect(await getCurrentFailoverCandidates()).to.have.length(2);
     expect(await getCurrentInSyncFollowers()).to.have.length(2);
 
     await shutdownFollower();
     // Failover cannot have happened yet
+    expect(await getCurrentFailoverCandidates()).to.have.length(2);
     expect(await getCurrentInSyncFollowers()).to.have.length(2);
     
     // 1 of 5 DBServers left, write to a collection should fail. Collection is now in read-only mode.
@@ -200,34 +276,6 @@ describe("Replication", function() {
   });
 
   it("collection should be set to read-only mode after nr of dbservers drops below minReplicationFactor - always dropping leader", async function() {
-    const validateROMode = async (availableFollowers, lastLeader) => {
-      let changedLeader = false;
-      while (true) {
-        const inSync = await getCurrentInSyncFollowers();
-        if (inSync[0] !== lastLeader) {
-          changedLeader = true;
-        }
-         // We are NEVER allowed to get to only 1 copy
-        expect(inSync).to.have.length.above(1);
-        if (changedLeader && inSync.length === availableFollowers) {
-          return;
-        }
-        try {
-          // TRY to insert
-          await db.collection(replicationCollectionName).save({ testung: Date.now() });
-          // If we can write we must be upgraded to more than 2
-          const inSync = await getCurrentInSyncFollowers();
-          expect(changedLeader).to.be.true;
-          expect(inSync).to.have.length.above(2);
-        } catch (e) {
-          expect(e.isArangoError).to.be.true;
-          expect(e.errorNum).to.equal(1004);
-          expect(e.statusCode).to.equal(403);
-        }
-        await sleep(1000);
-      }
-    };
-
     // 5 DBServers available, write to a collection should work flawlessly.
     expect(await getCurrentInSyncFollowers()).to.have.length(5);
     let c1 = await db.collection(replicationCollectionName).save({ testung: Date.now() });
@@ -235,6 +283,7 @@ describe("Replication", function() {
     expect(await getCurrentInSyncFollowers()).to.have.length(5);
     console.log("Shutdown first leader");
     let lastLeader = await shutdownLeader();
+    console.log("Shutdown first leader done " + lastLeader);
     await validateROMode(4, lastLeader);
 
     console.log("Shutdown second leader");
@@ -252,46 +301,21 @@ describe("Replication", function() {
         await db.collection(replicationCollectionName).save({ testung: Date.now() });
         expect.fail();
       } catch (e) {
+        if (!e.isArangoError) {
+          // Assertion!
+          throw e;
+        }
         expect(e.isArangoError).to.be.true;
         expect(e.errorNum).to.equal(1004);
         expect(e.statusCode).to.equal(403);
       }
-      expect(await getCurrentInSyncFollowers()).to.have.length(2);
+      // Keep the dead leader as potential FO candidate, if it raises back from the grave
+      expect(await getCurrentFailoverCandidates()).to.have.length(3);
       sleep(1000);
     }
   });
 
-  it("collection should recover from read-only mode after nr of dbservers rises above minReplicationFactor - always dropping leader", async function() {
-    const validateROMode = async (availableFollowers, lastLeader) => {
-      let changedLeader = false;
-      for (let i = 0; i < 100; ++i) {
-        const inSync = await getCurrentInSyncFollowers();
-        if (inSync[0] !== lastLeader) {
-          changedLeader = true;
-        }
-         // We are NEVER allowed to get to only 1 copy
-        expect(inSync).to.have.length.above(1);
-        if (changedLeader && inSync.length === availableFollowers) {
-          return;
-        }
-        try {
-          // TRY to insert
-          await db.collection(replicationCollectionName).save({ testung: Date.now() });
-          // If we can write we must be upgraded to more than 2
-          const inSync = await getCurrentInSyncFollowers();
-          expect(changedLeader).to.be.true;
-          expect(inSync).to.have.length.above(2);
-        } catch (e) {
-          expect(e.isArangoError).to.be.true;
-          expect(e.errorNum).to.equal(1004);
-          expect(e.statusCode).to.equal(403);
-        }
-        await sleep(1000);
-      }
-      // If we get here we did not manage to get back in sync in-time
-      expect.fail();
-    };
-
+    it("collection should recover from read-only mode after nr of dbservers rises above minReplicationFactor - always dropping leader", async function() {
     // 5 DBServers available, write to a collection should work flawlessly.
     expect(await getCurrentInSyncFollowers()).to.have.length(5);
     let c1 = await db.collection(replicationCollectionName).save({ testung: Date.now() });
@@ -299,32 +323,43 @@ describe("Replication", function() {
     expect(await getCurrentInSyncFollowers()).to.have.length(5);
     console.log("Shutdown first leader");
     const suspendedLeader = await suspendLeader();
-    console.log(`First leader ${suspendedLeader.name} is gone`);
-    await validateROMode(4, suspendedLeader.name);
+    let lastLeader;
+    try {
+      console.log(`First leader ${suspendedLeader.name} is gone`);
+      await validateROMode(4, suspendedLeader.name);
 
-    console.log("Shutdown second leader");
-    let lastLeader = await shutdownLeader();
-    await validateROMode(3, lastLeader);
-    console.log(`Second leader ${lastLeader} is gone`);
+      console.log("Shutdown second leader");
+      lastLeader = await shutdownLeader();
+      await validateROMode(3, lastLeader);
+      console.log(`Second leader ${lastLeader} is gone`);
 
-    console.log("Shutdown third leader");
-    lastLeader = await shutdownLeader();
-    console.log(`Third leader ${lastLeader} is gone`);
-    // We will never be able to leave the RO mode here
-    // await validateROMode(2, lastLeader);
-    console.log("In RO mode");
-    for (let i = 0; i < 10; ++i) {
-      try {
-        // TRY to insert
-        await db.collection(replicationCollectionName).save({ testung: Date.now() });
-        expect.fail();
-      } catch (e) {
-        expect(e.isArangoError).to.be.true;
-        expect(e.errorNum).to.equal(1004);
-        expect(e.statusCode).to.equal(403);
+      console.log("Shutdown third leader");
+      lastLeader = await shutdownLeader();
+      console.log(`Third leader ${lastLeader} is gone`);
+      // We will never be able to leave the RO mode here
+      // await validateROMode(2, lastLeader);
+      console.log("In RO mode");
+      for (let i = 0; i < 10; ++i) {
+        try {
+          // TRY to insert
+          await db.collection(replicationCollectionName).save({ testung: Date.now() });
+          expect.fail();
+        } catch (e) {
+          if (!e.isArangoError) {
+            // Assertion!
+            throw e;
+          }
+          expect(e.isArangoError).to.be.true;
+          expect(e.errorNum).to.equal(1004);
+          expect(e.statusCode).to.equal(403);
+        }
+        expect(await getCurrentFailoverCandidates()).to.have.length(3);
+        await sleep(1000);
       }
-      expect(await getCurrentInSyncFollowers()).to.have.length(2);
-      await sleep(1000);
+    } catch(e) {
+      // We need to continue the server, otherwise instance manager does not shutdown properly.
+      await continueServer(suspendedLeader.dbServer);
+      throw e;
     }
     console.log(`Continue First leader ${suspendedLeader.name}`);
     await continueServer(suspendedLeader.dbServer);
@@ -332,6 +367,7 @@ describe("Replication", function() {
     // We can now leave RO mode after suspended leader is reintegrated.
     // Also validate that the suspended leader does not harm current in any way.
     await validateROMode(3, lastLeader);
+
   });
 
   afterEach(() => afterEachCleanup(this, instanceManager));
